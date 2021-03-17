da607d9e39a581f7123d9ea57d26b0121f77e77b
Fabian Jetzinger <jetzinger.fabian@gmail.com>
2021-03-17 12:23:47 +0100

    basic pcacv implementation based on R code

--- resources/PCA.py
+++ resources/PCA.py
@@ -46,6 +46,11 @@ plt.show()
 #%% PCA
 import numpy as np
 import numpy.linalg as nplin
+from sklearn.utils.validation import check_array
+from sklearn.decomposition import TruncatedSVD
+from tqdm import tqdm
+from scipy.sparse.linalg import svds
+
 def pca_(X, ncomp='max', center=True, sing_vals=False):
     """
     Just PCA
@@ -91,3 +96,82 @@ def nanpca_(X, ncomp='max', center=True, tol=10e-12, max_iter=200, sing_vals=Fal
         return (scores, loadings, singulars, (iters, err, Z))
     else:
         return (scores, loadings, (iters, err, Z))
+
+
+def pcacv_(X, ncomp='max', center=True):
+    """
+    PCA with leave-one-out cross-validation
+    :param X:
+    :return:
+    """
+    # check if X is array
+    X = check_array(X)
+
+    n_samples, n_features = X.shape
+
+    # center X columns
+    if isinstance(center, bool) and center:
+        X = X - np.mean(X, axis=0)
+
+    # set ncomp
+    if ncomp == 'max':
+        ncomp = min(n_samples-1, n_features)
+    else:
+        ncomp = min(ncomp, min(n_samples-1, n_features))
+
+    # prepare storage
+    Xhat = np.zeros((n_samples, n_features, ncomp))
+
+    # Cross-validation (leave-one-out)
+    for i in tqdm(range(n_samples), leave=True):
+        Xi = np.delete(X, i, 0)
+
+        # sklearn truncated svd --> Pi is different, but same end result
+        svd = TruncatedSVD(ncomp)
+        svd.fit(Xi)
+        Pi = np.transpose(svd.components_)
+
+        # scipy svds --> different result
+        # u, s, vh = svds(Xi, ncomp, return_singular_vectors="vh")
+        # Pi = np.transpose(vh)
+
+        # old approach: complete SVD, not truncated --> calculates all components
+        # u, s, vh = nplin.svd(Xi, full_matrices=False, compute_uv=True)
+        # Pi = np.transpose(vh)
+
+        # repeat i-th row to create n_features*n_features matrix with 0 diagonal
+        Xii = np.array([X[i,:],]*n_features)
+        np.fill_diagonal(Xii, 0)
+
+        # Magic to avoid information bleed
+        PiP = np.transpose(np.cumsum(Pi**2, 1))
+        PiP1 = np.transpose(PiP/(1-PiP)+1)
+        PihP = np.transpose(Pi*(np.matmul(Xii, Pi)))
+
+        for j in range(n_features):
+            PP = np.matmul(PihP[:, j].reshape(ncomp, 1), PiP1[j, :].reshape(1, ncomp))
+            PP[np.tril_indices_from(PP, -1)] = 0
+
+            Xhat[i,j,:] = np.sum(PP, 0)
+
+    error = np.zeros(ncomp)
+    for i in range(ncomp):
+        error[i] = np.sum((X-Xhat[:,:,i])**2)
+
+    return error
+
+
+X = np.array([[1, 2, 3, 4, 5], [2, 1, 3, 3, 7], [3, 5, 5, 1, 8], [6, 7, 2, 3, 5], [9, 4, 7, 1, 6]], dtype='float64')
+#scores, loadings = pca_(X)
+result = pcacv_(X)
+print(str(result))
+
+
+#X1 = np.array([[1, 2, 3, 4], [2, 1, 3, 3], [3, 5, 5, 1]], dtype='float64')
+#X2 = np.array([[1, 2, 3], [2, 1, 3], [3, 5, 5], [7,1,1]], dtype='float64')
+
+# print(X1)
+# print()
+# print(X2)
+# print()
+# print(np.matmul(X2, X1))
